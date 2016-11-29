68bea39f95ebacf5a48481f674e4dbf104d9137b
abrilnastassja13 <abril.saravia13@gmail.com>
2016-11-29 11:07:13 -0300

    alerta en telefono

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
-/* teclado = function(ev) {
-	var ascii = ev.keycode;
-	if ((ascii>= 48 && ascii<=57) || ascii==37 || ascii==39 || ascii== 8) {
-			return true;
-	}	else {
+//Alerta para numero de telefono
+
+$(document).ready(function(){
+	// aca va todo lo ejecutable
+	$("#next").on("click", function(){
+		var numeroCelular = $("#numero-celular").val();
+		if (numeroCelular.length != 8) {
+			alert("Your number must contain 8 digits");
 			return false;
-	}
-}*/
+		}
 
-var celular = function() {
-	var maximoDigitos = $("#numero-celular").val();
-	if (maximoDigitos.length < 8) {
-		alert("Debe contener como máximo 8 digitos");
-	}
-	 return false;
-}
+	});
 
-var carga = function() {
-	$("#numero-celular").focus();
-	
-}
-
-$(document).ready(carga);
+});
